b9370e089e5f5cd37ef402fc8845db8d8e7b34bf
Ruy Vieira <ruy.vieiraneto@gmail.com>
2025-07-12 18:02:16 -0300

    fix: webhook update validation

--- src/webhook/model/update.go
+++ src/webhook/model/update.go
@@ -6,11 +6,11 @@ import (
 
 // UpdateWebhook represents the model for updating an existing webhook
 type UpdateWebhook struct {
-	Url           string `json:"url,omitempty" validate:"url"` // Optional updated URL, validated to be a valid URL
-	Authorization string `json:"authorization,omitempty"`      // Optional updated authorization token
-	Event         Event  `json:"event,omitempty"`              // Optional updated event associated with the webhook
+	Url           string `json:"url,omitempty" validate:"omitempty,url"`             // Optional updated URL, validated to be a valid URL
+	Authorization string `json:"authorization,omitempty"`                            // Optional updated authorization token
+	Event         Event  `json:"event,omitempty" validate:"omitempty,webhook_event"` // Optional updated event associated with the webhook
 	HttpMethod    string `json:"http_method,omitempty" validate:"omitempty,oneof=GET POST PUT DELETE PATCH"`
-	Timeout       *int   `json:"timeout,omitempty" validate:"omitempty,gte=1,leq=60"`
+	Timeout       *int   `json:"timeout,omitempty" validate:"omitempty,gte=1,lte=60"`
 
 	common_model.RequiredId
 }
