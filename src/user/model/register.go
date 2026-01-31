85a76f8bdeb685ecefa827133ba20a6e29d8672c
Ruy Vieira <ruy.vieiraneto@gmail.com>
2026-01-31 16:31:57 -0300

    refactor: better response types at password reset and email verification

--- src/user/model/register.go
+++ src/user/model/register.go
@@ -15,7 +15,15 @@ type ForgotPasswordRequest struct {
 	Email string `json:"email" validate:"required,email"`
 }
 
+type ForgotPasswordResponse struct {
+	Message string `json:"message" example:"If your email is registered, you will receive a password reset link"`
+}
+
 type ResetPasswordRequest struct {
 	Token    string `json:"token" validate:"required"`
 	Password string `json:"password" validate:"required,min=8,max=72"`
+}
+
+type ResetPasswordResponse struct {
+	Message string `json:"message" example:"Password reset successful"`
 }
